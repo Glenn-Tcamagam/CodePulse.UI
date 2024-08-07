a1a454e58358bf95dbc506da6ce893ee01e9f14b
Glenn-Tcamagam <glennwouagam@gmail.com>
2024-08-07 15:46:19 +0200

    end add and GET allBlogPost

---
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { Observable } from 'rxjs';
+
+import { BlogPost } from '../models/blog-post.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  constructor(private http: HttpClient) { }
+
+
+  
+  // A SAVOIR: LE NOM DE CONTROLLER dans .NET EST blogpostController  sans S  a la fin raison 
+  // pourlaquelle dans le service je fais api/blogpost       
+  //Alors que dans le cours de UDEMY cest /api/blogpost par ce que dans leur cours leur nom du controller finit pat s et est blogpostsController
+
+  createBlogPost(data: AddBlogPost) : Observable<BlogPost> {
+    return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/blogpost`, data);
+  }
+
+  getAllBlogPosts() : Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogpost`);
+  }
+
+}
